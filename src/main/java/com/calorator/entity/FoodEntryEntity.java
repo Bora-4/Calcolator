8d0ad30c52f6a8a5937c2ddb5aba2736a4cac496
Bora-4 <bora.rrena@fti.edu.al>
2024-12-29 20:17:03 +0100

    implementimi i FoodEntryEntity.java

--- src/main/java/com/calorator/entity/FoodEntryEntity.java
+++ src/main/java/com/calorator/entity/FoodEntryEntity.java
@@ -1,4 +1,57 @@
 package com.calorator.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "food_entries")
 public class FoodEntryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "entry_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(name = "food_name", nullable = false)
+    private String foodName;
+
+    @Column(nullable = false)
+    private Integer calories;
+
+    private Double price;
+
+    @Column(name = "entry_date", nullable = false)
+    private LocalDateTime entryDate;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PostPersist
+    protected void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
+
+
+
 }
