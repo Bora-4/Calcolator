6a2f6854219eff4195c0b5499189d564055846a5
Bora-4 <bora.rrena@fti.edu.al>
2024-12-30 13:57:40 +0100

    Ne interface-in FoodEntryRepository percaktoj metodat te cilat nderveprojne ne menyre te drejteperdrejte me databazen ku vecoj metodat filtruese te te dhenave si entryDateFiltering, findFoodEntriesLast7Days, countFoodEntriesLast7Days.

--- src/main/java/com/calorator/repository/FoodEntryRepository.java
+++ src/main/java/com/calorator/repository/FoodEntryRepository.java
@@ -1,4 +1,20 @@
 package com.calorator.repository;
 
+import com.calorator.entity.FoodEntryEntity;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
 public interface FoodEntryRepository {
+    void save(FoodEntryEntity foodEntry);
+    FoodEntryEntity findById(Long id);
+    FoodEntryEntity findByName(String foodName);
+    List<FoodEntryEntity> entryDateFiltering(Long userId, LocalDateTime startDate, LocalDateTime endDate);
+    List<FoodEntryEntity> findFoodEntriesLast7Days();
+    Long countFoodEntriesLast7Days();
+    void update(FoodEntryEntity foodEntry);
+    List<FoodEntryEntity> findAll();
+    void delete(Long id);
 }
