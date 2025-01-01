00c068a875c5707b2247c721a28586d4286b57db
Rei Gjuzi <reigjuzi01@gmail.com>
2025-01-01 18:13:25 +0100

    Update security rules and configurations

--- src/main/java/com/calorator/config/SecurityConfig.java
+++ src/main/java/com/calorator/config/SecurityConfig.java
@@ -1,31 +1,24 @@
 package com.calorator.config;
 
-import com.calorator.service.impl.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/signup", "/login", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/signup", "/login", "/home", "/users/**", "/css/**", "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout(logout -> logout
-                        .permitAll()
-                );
+                .csrf(csrf -> csrf.disable()) // Optional: Disable CSRF if needed
+                .httpBasic(httpBasic -> httpBasic.disable()); // Disable basic auth if not needed
         return http.build();
     }
+
 }
