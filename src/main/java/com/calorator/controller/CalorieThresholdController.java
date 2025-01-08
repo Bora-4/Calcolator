8ebe7dc2685dcd0e81cccbf7babf72341b4c06ec
Rei Gjuzi <reigjuzi01@gmail.com>
2025-01-08 14:51:05 +0100

    Rregullon mapping

--- src/main/java/com/calorator/controller/CalorieThresholdController.java
+++ src/main/java/com/calorator/controller/CalorieThresholdController.java
@@ -10,7 +10,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/calorie-threshold")
+@RequestMapping("/calorie-threshold")
 public class CalorieThresholdController {
 
     @Autowired
