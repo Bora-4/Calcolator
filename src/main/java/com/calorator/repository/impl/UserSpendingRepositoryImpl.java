fddca1837131f674db1bddc17bfc896ac3930306
Fabian Zerka <fabianzerka@gmail.com>
2025-01-20 21:59:18 +0100

    userSpending 3 layer logic

--- src/main/java/com/calorator/repository/impl/UserSpendingRepositoryImpl.java
+++ src/main/java/com/calorator/repository/impl/UserSpendingRepositoryImpl.java
@@ -1,4 +1,47 @@
 package com.calorator.repository.impl;
 
-public class UserSpendingRepositoryImpl {
+import com.calorator.entity.UserSpendingEntity;
+import com.calorator.repository.UserSpendingRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public class UserSpendingRepositoryImpl implements UserSpendingRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void save(UserSpendingEntity userSpendingEntity) {
+        entityManager.persist(userSpendingEntity);
+    }
+
+    @Override
+    public void update(UserSpendingEntity userSpendingEntity) {
+        entityManager.merge(userSpendingEntity);
+    }
+
+    @Override
+    public void delete(Long spendingId) {
+        UserSpendingEntity entity = findById(spendingId);
+        if (entity != null) {
+            entityManager.remove(entity);
+        }
+    }
+
+    @Override
+    public UserSpendingEntity findById(Long spendingId) {
+        return entityManager.find(UserSpendingEntity.class, spendingId);
+    }
+
+    @Override
+    public List<UserSpendingEntity> findAll() {
+        String query = "SELECT u FROM UserSpendingEntity u";
+        return entityManager.createQuery(query, UserSpendingEntity.class).getResultList();
+    }
 }
