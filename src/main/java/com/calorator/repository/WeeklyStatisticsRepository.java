539f732cfd8395aaf19b475c24fd364132ff3856
Riona <riona.fida@gmail.com>
2025-01-21 02:12:00 +0100

    WeeklyStatistics java classes

--- src/main/java/com/calorator/repository/WeeklyStatisticsRepository.java
+++ src/main/java/com/calorator/repository/WeeklyStatisticsRepository.java
@@ -1,4 +1,9 @@
 package com.calorator.repository;
 
-public interface WeeklyStatisticsRepository {
+import com.calorator.entity.WeeklyStatisticsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WeeklyStatisticsRepository extends JpaRepository<WeeklyStatisticsEntity, Long> {
 }
