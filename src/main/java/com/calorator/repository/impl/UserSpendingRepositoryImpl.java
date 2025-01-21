265818dff884bb745f48967bb2ff7e4980e32a27
Bora Rrena <135857113+Bora-4@users.noreply.github.com>
2025-01-21 20:02:42 +0100

    Update UserSpendingRepositoryImpl.java
    Annotation @Transactional nuk duhet vendosur tek Repository Layer por duhet vendosur tek Service Layer

--- src/main/java/com/calorator/repository/impl/UserSpendingRepositoryImpl.java
+++ src/main/java/com/calorator/repository/impl/UserSpendingRepositoryImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-@Transactional
+
 public class UserSpendingRepositoryImpl implements UserSpendingRepository {
 
     @PersistenceContext
