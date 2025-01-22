5c308fb2a1b84218b8f0d279013f65774d8fdc89
Katerina Kaloshi <katerinakaloshi1@gmail.com>
2025-01-22 17:48:25 +0100

    tek service, delete dhe update statistikat javore

--- src/main/java/com/calorator/service/WeeklyStatisticsService.java
+++ src/main/java/com/calorator/service/WeeklyStatisticsService.java
@@ -5,23 +5,10 @@ import java.util.List;
 
 public interface WeeklyStatisticsService {
 
-    WeeklyStatisticsDTO createWeeklyStatistics(WeeklyStatisticsDTO weeklyStatisticsDTO, Long reportId);
-
-    WeeklyStatisticsDTO updateWeeklyStatistics(Long id, WeeklyStatisticsDTO weeklyStatisticsDTO);
-
-    WeeklyStatisticsDTO getWeeklyStatisticsById(Long id);
-
-    List<WeeklyStatisticsDTO> getAllWeeklyStatistics();
-
     void saveWeeklyStatistics(WeeklyStatisticsDTO weeklyStatisticsDTO);
-
-    void updateWeeklyStatistics(WeeklyStatisticsDTO weeklyStatisticsDTO);
-
+    WeeklyStatisticsDTO findWeeklyStatisticsById(Long id);
+    List<WeeklyStatisticsDTO> findAllWeeklyStatistics();
+    void updateWeeklyStatistics( WeeklyStatisticsDTO weeklyStatisticsDTO);
     void deleteWeeklyStatistics(Long id);
 
-    WeeklyStatisticsDTO findWeeklyStatisticsById(Long statisticId);
-
-    List<WeeklyStatisticsDTO> findAllWeeklyStatistics();
-
-    WeeklyStatisticsDTO findWeeklyStatisticsByStatisticName(String statisticName);
 }
