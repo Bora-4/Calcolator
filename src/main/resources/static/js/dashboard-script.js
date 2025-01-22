67612bdf40c133b905938970cb8827f75af6e756
Rei Gjuzi <reigjuzi01@gmail.com>
2025-01-22 20:05:13 +0100

    Refactor

--- src/main/resources/static/js/dashboard-script.js
+++ src/main/resources/static/js/dashboard-script.js
@@ -144,14 +144,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-function openModal() {
-    document.getElementById('foodEntryModal').style.display = "block";
-}
-
-function closeModal() {
-    document.getElementById('foodEntryModal').style.display = "none";
-}
-
 document.getElementById('viewWeeklySummaryButton').addEventListener('click', function() {
     let weeklyData = [
         { day: 'Monday', calories: 2500, thresholdExceeded: true },
